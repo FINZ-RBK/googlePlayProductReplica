d63a15a1af58388b776e2ec937d2b015f6479b44
islamYMansour <islamy95@hotmail.com>
2019-11-09 17:08:36 +0200

    init

--- slider-service/slider/server/index.js
+++ slider-service/slider/server/index.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+const port = process.env.PORT || 3002;
+
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 // var items = require('../database-mysql');
 // var items = require('../database-mongo');
@@ -12,10 +14,13 @@ app.use(
     })
 );
 
+app.get("*", (req, res) => {
+    res.sendFile(express.static(__dirname + "/../react-client/dist/index.html"));
+});
 // UNCOMMENT FOR REACT
 app.use(express.static(__dirname + "/../react-client/dist"));
 app.use("/", routes);
 
-app.listen(3002, function() {
+app.listen(port, function() {
     console.log("listening on port 3002!");
 });
