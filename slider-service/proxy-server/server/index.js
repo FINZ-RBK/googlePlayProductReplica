3b2c638668cc9a6b94a1e13d9b3396ae22a1ff03
islamYMansour <islamy95@hotmail.com>
2019-11-09 17:14:21 +0200

    update

--- slider-service/proxy-server/server/index.js
+++ slider-service/proxy-server/server/index.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+const port = process.env.PORT || 3007;
+
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 // var items = require('../database-mysql');
 // var items = require('../database-mongo');
@@ -14,9 +16,12 @@ app.use(
 app.use(express.static(__dirname + "/../react-client/dist"));
 
 app.use("/", routes);
-
+app.get("*", (req, res) => {
+    res.sendFile(express.static(__dirname + "/../react-client/dist/index.html"));
+});
 // UNCOMMENT FOR REACT
+// .listen(process.env.PORT || 5000)
 
-app.listen(3007, function() {
+app.listen(port, function() {
     console.log("listening on port 3007 proxy!");
 });
