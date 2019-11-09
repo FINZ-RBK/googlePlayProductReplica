494bb73f3c39aa5f0141d8e128ea76c277d93435
FatoomMFayad <fatoomfayyad@gmail.com>
2019-11-09 19:05:35 +0200

    basic-info- service init

---
+++ basic-info-service/proxy-server/react-client/src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      items: []
+    }
+  }
+  render () {
+    return (<div>
+      <h1>Basic information</h1>
+      <div id="basic"></div>
+    </div>)
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('proxy-basic'));
