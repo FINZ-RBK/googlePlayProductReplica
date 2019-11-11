eba6205a66b81f87a33693f7222eca5a2d8cb59a
FatoomMFayad <fatoomfayyad@gmail.com>
2019-11-11 17:57:10 +0200

    add connection

--- basic-info-service/proxy-server/server/index.js
+++ basic-info-service/proxy-server/server/index.js
@@ -1,21 +1,26 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 
 const routes = require("./routes/index");
 const port = process.env.PORT || 3001;
+const URI = process.env.mongoURI || "mongodb+srv://fatoom:fatoom@cluster0-hft43.mongodb.net/basic-info?retryWrites=true&w=majority";
+mongoose.connect(URI);
 
 //Initialize Express
 var app = express();
 //Parse request body as JSON
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+monogoo
 
 //Make public static folder 
-app.use('/', routes);
+//app.use('/', routes);
 
 // UNCOMMENT FOR REACT
 app.use(express.static(__dirname + '/../react-client/dist'));
+
 
 //Start the server 
 app.listen(port, function() {
