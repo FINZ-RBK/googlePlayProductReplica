ed08acf31e93944583159f57e61fd40052168c7b
FatoomMFayad <fatoomfayyad@gmail.com>
2019-11-11 17:41:57 +0200

    i

--- basic-info-service/proxy-server/server/index.js
+++ basic-info-service/proxy-server/server/index.js
@@ -3,11 +3,10 @@ var bodyParser = require('body-parser');
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 
 const routes = require("./routes/index");
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3001;
 
 //Initialize Express
 var app = express();
-
 //Parse request body as JSON
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
@@ -20,6 +19,6 @@ app.use(express.static(__dirname + '/../react-client/dist'));
 
 //Start the server 
 app.listen(port, function() {
-  console.log('listening on port ' + PORT + '!');
+  console.log('listening on port ' + port + '!');
 });
 
