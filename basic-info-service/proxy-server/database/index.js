1382011e3042782dd2fb6bbe865fafaad6a18d8c
Fatoom Fayad <fatoomfayyad@gmail.com>
2020-01-26 19:12:51 +0200

    Update index.js

--- basic-info-service/proxy-server/database/index.js
+++ basic-info-service/proxy-server/database/index.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
+//Database URI
 const URI = process.env.mongoURI || "mongodb+srv://<user>:<password>@cluster0-hft43.mongodb.net/basic-info?retryWrites=true&w=majority";
 mongoose.connect(URI, {useNewUrlParser: true ,  useUnifiedTopology: true});
 
+//Connect to Database
 var db = mongoose.connection;
 var Product =  require('./models/Product');
 
@@ -13,18 +15,18 @@ db.once('open', function() {
   console.log('mongoose connected successfully');
 });
 
+//get products depending on the given query
 var retriveData = function(query, callback) {
   var options = {};
   if (query) {
       options = query;
   }
-
   Product.find(options, (err, data) => {
-    console.log("ddd", data);
       callback(err, data);
   });
 };
 
+// export Product model and retrieveData 
 module.exports = {
   Product: require('./models/Product'),
   retriveData: retriveData
