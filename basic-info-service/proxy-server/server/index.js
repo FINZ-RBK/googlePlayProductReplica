d2323b9f73a63fbb2b0d7ccf874ae182b45393a8
Fatoom Fayad <fatoomfayyad@gmail.com>
2020-01-27 09:52:11 +0200

    Update index.js

--- basic-info-service/proxy-server/server/index.js
+++ basic-info-service/proxy-server/server/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require("path");
 var bodyParser = require('body-parser');
-// UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 
 const routes = require("./routes/index");
 const port = process.env.PORT || 3001;
@@ -12,6 +11,7 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname , '../react-client/dist')));
+//Allow the express server to use routes
 app.use("/", routes);
 
 //Start the server 
